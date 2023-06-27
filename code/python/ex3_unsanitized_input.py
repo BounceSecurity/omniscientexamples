017a8c919476a613bafa84110d9c4b8ca56499c1
Josh Grossman (Bounce Security) <97975715+joshbouncesecurity@users.noreply.github.com>
2023-06-27 16:25:50 +0300

    Add ex3

---
+++ code/python/ex3_unsanitized_input.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def sanitize_it(user_input):
+    #Sanitize the input to avoid code execution
+    return user_input
+
+def get_input():
+    #some actions before getting an input
+    return input("please enter address")
+
+def run_ping(address):
+    cmd = "ping -c 1 %s" % address
+    subprocess.Popen(cmd, shell=True)
+
+new_address = get_input()
+
+#ruleid: unsanitized_input
+run_ping(new_address)
+
+new_address = sanitize_it(new_address)
+
+#ok: unsanitized_input
+run_ping(new_address)
